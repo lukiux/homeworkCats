7fb4f2e095264a72e02707cbf70a7f97d0e60045
Lukas <lukaukas@gmail>
2020-05-06 11:25:59 +0300

    generated json file

--- app/Services/CatService.php
+++ app/Services/CatService.php
@@ -2,8 +2,12 @@
 
 namespace App\Services;
 
+use App\Visitor;
+
 class CatService {
 
+    CONST CACHE_KEY = 'CATS';
+    
     protected function readFile() {
 
         $cats = array();
@@ -26,5 +30,25 @@ class CatService {
         $combCats = array($cats[$rand_cats[0]], $cats[$rand_cats[1]], $cats[$rand_cats[2]]);
 
         return $combCats;
+    }
+
+    public function getCachedKey($id) {
+        $key = strtoupper("get.{$id}");
+        return self::CACHE_KEY . ".$key";
+    }
+
+    public function generateLogFile($catsCom, $n) {
+
+        $dataJSON = array();
+
+        $dataJSON['datetime'] = date(Visitor::where('page_id', $n)->latest()->first()->updated_at);
+        $dataJSON['N'] = $n;
+        $dataJSON['Cats'] = $catsCom;
+        $dataJSON['countAll'] = Visitor::all()->count();
+        $dataJSON['countN'] = Visitor::where('page_id', $n)->count();
+
+        $fp = fopen('../log.json', 'w');
+        fwrite($fp, json_encode($dataJSON));
+        fclose($fp);
     }
 }
