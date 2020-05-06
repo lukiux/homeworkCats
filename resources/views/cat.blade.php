097dd2042d0a84a3cabed0713d115885ef145781
Lukas <lukaukas@gmail>
2020-05-06 11:28:47 +0300

    deleted session on view page

--- resources/views/cat.blade.php
+++ resources/views/cat.blade.php
@@ -1,5 +1,17 @@
 <?php
 
+/*session_start();
+
+if( isset( $_SESSION['counter'] ) ) {
+    $_SESSION['counter'] += 1;
+ }else {
+    $_SESSION['counter'] = 1;
+ }
+
+echo "CountAll: " . $_SESSION['counter'] . "<br/>";*/
+
+
+
 foreach ($catsComb as $key => $catComb) {
     
     if ($key == count($catsComb) - 1) {
